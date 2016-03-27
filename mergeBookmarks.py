693fa999e4bad961a1971fd4d05a61959c219ffb
Mario Diana <software@mariodiana.com>
2016-03-26 20:56:41 -0400

    Initial commit for mergeBookmarks.py.

---
+++ mergeBookmarks.py
@@ -0,0 +1,54 @@
+import glob
+import json
+import os.path
+
+
+def convertBookmarks(deliciousBookmarks):
+    bookmarks = []
+    for bm in deliciousBookmarks:
+        # Some of my bookmarks have a malformed tag.
+        tags = list(map(lambda x: x if x != "UI/UX" else "UI-UX", bm['t']))
+        bookmark = dict()
+        bookmark['tags'] = tags
+        bookmark['url'] = bm['u']
+        bookmark['description'] = bm['d']
+        bookmark['dt'] = bm['dt']
+        bookmark['extended'] = bm['n']
+        bookmarks.append(bookmark)
+    return bookmarks
+
+
+def mergeBookmark(dictionary, bookmarks):
+    for bm in bookmarks:
+        url = bm['url']
+        if url in dictionary:
+            a = set(dictionary[url]['tags'])
+            b = set(bm['tags'])
+            a.update(b)
+            dictionary[url]['tags'] = list(a)
+        else:
+            dictionary[url] = bm
+    return dictionary
+
+
+def main(files):
+    bookmarks = {}
+    for aFile in files:
+        print(aFile)
+        data = None
+        with open(aFile, 'r') as f:
+            data = f.read()
+        bookmarksForTag = json.loads(data)
+        convertedBookmarks = convertBookmarks(bookmarksForTag)
+        bookmarks = mergeBookmark(bookmarks, convertedBookmarks)
+    allBookmarks = []
+    for aKey in bookmarks.keys():
+        allBookmarks.append(bookmarks[aKey])
+    data = json.dumps(allBookmarks)
+    with open('bookmarks.json', 'w') as f:
+        f.write(data)
+
+
+if __name__ == '__main__':
+    files = glob.glob(os.path.join('data', '*.json'))
+    main(files)
